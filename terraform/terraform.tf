c917bc57699c88d003aebeafcdd07cb6e49a6560
cris33rj <cris33rj@gmail.com>
2026-02-08 12:16:13 -0300

    acho que agora vai

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,11 +29,12 @@ terraform {
   backend "s3" {
     bucket = "vprofileactions5000"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   #required_version = "~> 1.6.3"
 }
+##
 ##
 ##
 ##
